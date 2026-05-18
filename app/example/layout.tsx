a7331ae3904e74253d63718699846a51a4143736
albert <albert.maharjan94@gmail.com>
2026-05-18 09:28:00 +0545

    may 18

---
+++ app/example/layout.tsx
@@ -0,0 +1,13 @@
+export default function Layout({children}: {children: React.ReactNode}) {
+    return (
+        <section>
+            <div className="border p-4">Example Header</div>
+                {children}
+            <div className="border p-4">Example Footer</div>
+            
+            <div className="border p-4 bg-gray-100">
+                {children}
+            </div>
+        </section>
+    );
+}
