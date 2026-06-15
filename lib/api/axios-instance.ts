fbf5d161cacfeb2059fccf63344e7c1d51c9c74d
albert <albert.maharjan94@gmail.com>
2026-06-15 08:19:37 +0545

    jun 15

--- lib/api/axios-instance.ts
+++ lib/api/axios-instance.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getTokenCookie } from "../cookies";
 const BASE_URL = 
     process.env.NEXT_PUBLIC_API_URL 
     || "http://localhost:8088";
@@ -10,4 +11,16 @@ const axiosInstance = axios.create({
     },
 });
 
+axiosInstance.interceptors.request.use(
+    async (config) => {
+        const token = await getTokenCookie();
+        if (token) {
+            config.headers["Authorization"] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 export default axiosInstance;
